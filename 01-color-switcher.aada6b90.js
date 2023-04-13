217926abb0ff60aa6f9b71b72eb6d5092b5e2a96
anderson03AyA <116131461+anderson03AyA@users.noreply.github.com>
2023-04-13 05:12:40 +0000

    Deploying to gh-pages from @ anderson03AyA/goit-js-hw-09@42aa9daf98f135c5c3b2e0acbd6b4d58e8a6263f 🚀

---
+++ 01-color-switcher.aada6b90.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),a=document.querySelector("body");let d=null;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{var e;e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,color=e,a.style.display="inline",a.style.background=color}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.aada6b90.js.map
